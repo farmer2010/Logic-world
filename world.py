7f24ca47d3b8e868bb95bee09876d292cea8f6f0
farmer2010 <155635633+farmer2010@users.noreply.github.com>
2025-03-16 22:32:09 +1000

    Add files via upload

--- world.py
+++ world.py
@@ -137,7 +137,7 @@ class World:
                             self.timer = 0
                             bl = block.get_block(self, blockpos, self.inventory_names[self.inventory_index])
                             sl = self.inventory_names[self.inventory_index]
-                            if sl == "NOT" or sl == "AND" or sl == "XOR" or sl == "diode" or sl == "output":
+                            if sl == "NOT" or sl == "AND" or sl == "XOR" or sl == "diode" or sl == "output" or sl == "memory":
                                 bl.data["rotate"] = self.select_rotate
                             bl.connect_with_armored_wire()
                             self.change_image()
